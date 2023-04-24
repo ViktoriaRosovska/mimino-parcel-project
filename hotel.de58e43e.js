c4fdbe868136082d444dff0b943e46fa62ef5903
ViktoriaRosovska <121065008+ViktoriaRosovska@users.noreply.github.com>
2023-04-24 20:13:57 +0000

    Deploying to gh-pages from @ ViktoriaRosovska/mimino-parcel-project@8a6cf0dace6a3efe92585642f426e6fece19d03a 🚀

---
+++ hotel.de58e43e.js
@@ -0,0 +1,2 @@
+new Swiper(".hotel-hero__slider",{loop:!0,spaceBetween:20,autoplay:{delay:2500,disableOnInteraction:!1},slidesPerView:1.5,breakpoints:{375:{slidesPerView:1.5},768:{slidesPerView:1.5},1440:{slidesPerView:1.5}},navigation:{nextEl:".swiper-button-next-1",prevEl:".swiper-button-prev-1"}});
+//# sourceMappingURL=hotel.de58e43e.js.map
--- hotel.ec85573e.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".hotel-hero__slider",{spaceBetween:20,slidesPerView:1.5,slidesPerGroup:1,allowSlidePrev:!0,allowSlideNext:!0,resizeObserver:!1,loop:!0,loopedSlides:3,autoplay:{delay:2500,stopOnLastSlide:!1,disabledOnIteration:!1},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},speed:600});
-//# sourceMappingURL=hotel.ec85573e.js.map
